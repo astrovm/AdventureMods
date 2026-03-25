ff272b531b25820608e68f9ae3bf77adf7c4a7f3
astrovm <~@4st.li>
2026-03-25 12:01:53 -0300

    chore: Remove development suffixes from APP_ID and VERSION, and update PROFILE to default.

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,8 @@
-// Build-time constants, overridden by meson configure_file in production builds.
-// These defaults allow `cargo build` to work outside meson.
-pub const APP_ID: &str = "io.github.astrovm.AdventureMods.Devel";
-pub const VERSION: &str = "0.1.0-devel";
+pub const APP_ID: &str = "io.github.astrovm.AdventureMods";
+pub const VERSION: &str = "0.1.0";
 #[allow(dead_code)]
 pub const GETTEXT_PACKAGE: &str = "adventure-mods";
 #[allow(dead_code)]
 pub const LOCALEDIR: &str = "/usr/share/locale";
 pub const PKGDATADIR: &str = "/usr/share/adventure-mods";
-pub const PROFILE: &str = "development";
+pub const PROFILE: &str = "default";
