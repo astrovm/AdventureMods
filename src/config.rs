ffc7a84132396131efd86877de7b11823366c1cf
astrovm <~@4st.li>
2026-04-11 21:27:05 -0300

    chore: update version to 0.3.4 in meson.build and config.rs

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 pub const APP_ID: &str = "io.github.astrovm.AdventureMods";
 pub const APP_NAME: &str = "Adventure Mods";
-pub const VERSION: &str = "0.3.3";
+pub const VERSION: &str = "0.3.4";
 pub const PKGDATADIR: &str = "/usr/share/adventure-mods";
 pub const PROFILE: &str = "default";
