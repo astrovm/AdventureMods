0f5873946b9890c3868f2615cc093dcc074eb736
astrovm <~@4st.li>
2026-07-22 17:14:48 -0300

    Make Flatpak smoke test container-safe

--- build-aux/test-flatpak-package.sh
+++ build-aux/test-flatpak-package.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -eu
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 BUILD_DIR MANIFEST APP_ID" >&2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 BUILD_DIR APP_ID" >&2
     exit 2
 fi
 
 build_dir=$1
-manifest=$2
-app_id=$3
+app_id=$2
 files_dir="$build_dir/files"
 
 test -x "$files_dir/bin/adventure-mods"
@@ -22,4 +21,4 @@ desktop-file-validate "$files_dir/share/applications/$app_id.desktop"
 appstreamcli validate --no-net "$files_dir/share/metainfo/$app_id.metainfo.xml"
 glib-compile-schemas --strict --dry-run "$files_dir/share/glib-2.0/schemas"
 
-flatpak-builder --run "$build_dir" "$manifest" adventure-mods --version
+"$files_dir/bin/adventure-mods" --version
